349463e9f6f68f8df74ca7a0ff83dcc1cd0fb279
Samih Abdeljabbar <abdels6375@gmail.com>
2020-05-09 04:56:06 -0500

    calling the global API

--- globalapi.py
+++ globalapi.py
@@ -1,1 +1,12 @@
+import json
+import requests
+
+global_url = "/v1/global-metrics/quotes/latest"
+
+request = requests.get(global_url)
+results = request.json()
+
+print(json.dumps(results, sort_keys=True, indent =4))
+
+
 
